cee55e5f1a3931aff203a615cec7e82569d7e46c
unknown <SebonK2255@gmail.com>
2022-04-23 14:42:46 +0200

    add kafka ui

---
+++ kafka_consumer.py
@@ -0,0 +1,14 @@
+from kafka import KafkaConsumer
+from json import loads
+
+consumer = KafkaConsumer(
+    'temperature',
+    bootstrap_servers=['localhost:9092'],
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='temperature',
+    value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+for message in consumer:
+    message = message.value
+    print('read {}'.format(message))
