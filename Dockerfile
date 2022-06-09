20fc70a18999e480b1bd9ea952ea08b86abb7e40
unknown <SebonK2255@gmail.com>
2022-06-09 17:06:51 +0200

    cleanup

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-# this is used to build image for the rabbit to kafka connector
-FROM confluentinc/cp-kafka-connect-base:5.2.1
-RUN confluent-hub install confluentinc/kafka-connect-rabbitmq:latest --no-prompt
--- helpers/test.py
+++
@@ -1,8 +0,0 @@
-from http import server
-from pymongo import MongoClient
-
-client = MongoClient(port=27017, username="admin", password="admin")
-db=client.admin
-# Issue the serverStatus command and print the results
-serverStatusResult=db.temperature.insert_one({"temperature":12})
-print(serverStatusResult)
